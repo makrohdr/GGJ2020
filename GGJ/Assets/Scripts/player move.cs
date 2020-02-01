835743d2a63d7f10e0e464e6f8c7a74746426765
Marco <makrohdr@hotmail.com>
2020-01-31 22:22:35 -0800

    Proyecto GGJ

---
+++ GGJ/Assets/Scripts/player move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermove : MonoBehaviour
+{
+    public float Salto = 2.5f;
+    public float Velocidad = 2.5f;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float MovHorizontal = Input.GetAxisRaw("Horizontal");
+    }
+    
+}
